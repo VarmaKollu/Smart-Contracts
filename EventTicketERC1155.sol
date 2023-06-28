53d620a1ee9685a69883114d098f8ab93aab08be
Varma Kollu <varmaprince24@yahoo.com>
2023-06-28 14:22:51 +0530

    commit

--- EventTicketERC1155.sol
+++ EventTicketERC1155.sol
@@ -1,1 +1,15 @@
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract EventTicket is ERC1155, Ownable {
+
+    // Ticket Data Structure
+    struct Ticket {
+        unint256 price;
+        unint256 maxPerBatch
+    }
+
+}
