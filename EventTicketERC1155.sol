cb92936a67530cf14f004c20163ecc7e3baeeb12
Varma Kollu <varmaprince24@yahoo.com>
2023-06-28 14:35:10 +0530

    commit

--- EventTicketERC1155.sol
+++ EventTicketERC1155.sol
@@ -9,7 +9,18 @@ contract EventTicket is ERC1155, Ownable {
     // Ticket Data Structure
     struct Ticket {
         unint256 price;
-        unint256 maxPerBatch
+        unint256 maxPerBatch;
+        unint256 totalMinted;
     }
+
+    // Mapping of Ticket ID to ticket details
+    mapping(unint256 => Ticket) public tickets;
+
+    // USDC contract address
+    address public usdcAddress;
+
+    // Events
+    event TicketMinted(uint256 indexed id, uint256 amount, uint256 price, uint256 maxPerBatch);
+    event maxPerBatchUpdated(uint256 indexed id, ) 
 
 }
